872eb8094ad5835f7d9d3a30ddcae23765e436a4
JDEAN92 <jdh92229@gmail.com>
2017-04-05 20:16:03 +0900

    Add files via upload

---
+++ JavaExample/src/com/dremingdean/javaee/MethodStructure.java
@@ -0,0 +1,12 @@
+package com.dremingdean.javaee;
+
+public class MethodStructure {
+
+	
+	int add(int x, int y){
+		int result = x + y;
+		return result;        
+		/*메소드를 호출한 곳으로 
+		x+y의 결과가 담긴 result를 반환한다.*/
+	}
+}
