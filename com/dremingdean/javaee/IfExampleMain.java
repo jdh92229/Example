2b499300a2ce573de8f48828e38f24935c29e7df
JDEAN92 <jdh92229@gmail.com>
2017-04-05 20:22:23 +0900

    Add files via upload

---
+++ com/dremingdean/javaee/IfExampleMain.java
@@ -0,0 +1,11 @@
+package com.dremingdean.javaee;
+
+public class IfExampleMain {
+
+	public static void main(String[] args) {
+		    IfExample result =new IfExample();
+			result.ifStatement(80);
+	}
+
+}
+
