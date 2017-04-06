6d43c31e9b595b9ae73b5d5111d5c4fa94539e38
JDEAN92 <jdh92229@gmail.com>
2017-04-06 16:03:17 +0900

    Add files via upload

--- com/dremingdean/javaee/IfExample.java
+++ com/dremingdean/javaee/IfExample.java
@@ -3,26 +3,26 @@ package com.dremingdean.javaee;
 import java.util.Scanner;
 
 public class IfExample {
-		int score;
+	int score;
 
-		public void ifStatement(){
-		
+	public void ifStatement(){
+
 		System.out.println("점수를 입력해 주세요");
-		
+
 		Scanner scanner = new Scanner(System.in);
 		String tmp = scanner.nextLine();  // 화면을 통해 입력받은 내용을 tmp에 저장
 		score = Integer.parseInt(tmp);  // 입력받은 tmp의 문자열을 숫자로 변환
-			
+
 		if(score>90){
 			System.out.println("A 입니다.");
 		}else if(score>80){
 			System.out.println("B 입니다.");
 		}else if(score>70){
-		System.out.println("C 입니다.");
+			System.out.println("C 입니다.");
 		}else{
 			System.out.println("F 입니다.");
 		}
 	}
-		
+
 }
 
--- com/dremingdean/javaee/IfExampleMain.java
+++ com/dremingdean/javaee/IfExampleMain.java
@@ -3,9 +3,9 @@ package com.dremingdean.javaee;
 public class IfExampleMain {
 
 	public static void main(String[] args) {
-		
-		    IfExample result =new IfExample();
-			result.ifStatement();
+
+		IfExample result =new IfExample();
+		result.ifStatement();
 	}
 
 }
